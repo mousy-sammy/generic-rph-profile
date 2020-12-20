4482cc87ce34071f20cf42dcfda66ceef29c166f
Nick Balsaras <nick@just-nick.com>
2020-12-20 15:14:05 +1100

    Add style

--- src/profile.tsx
+++ src/profile.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export const Profile: React.FunctionComponent = () => <>
+export const Profile: React.FunctionComponent = () => (
+  <div>
     <h1>Hello World</h1>
-</>
+    <style jsx>{`
+      div {
+        padding: 30px 0 0;
+        background: #ccc;
+      }
+    `}</style>
+  </div>
+);
